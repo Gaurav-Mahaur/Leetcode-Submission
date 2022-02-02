7ac68404b85d75f00249e75df283495caf69824c
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-02-03 00:22:39 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (45.08%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -28,7 +28,18 @@ public:
             return nums[0];
         }
         vector<int>dp(n,-1);
-        return fun(n-1,nums,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++)
+        {
+            int pick = nums[i];
+            if(i>1)
+            {
+                pick += dp[i-2];
+            }
+            int notpick = 0+dp[i-1];
+            dp[i] = max(pick,notpick);
+        }
+        return dp[n-1];
         
     }
 };
