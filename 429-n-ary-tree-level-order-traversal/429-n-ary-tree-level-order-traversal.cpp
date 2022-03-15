3abdf1fd5e246c51700f337b765cc9771d136b2a
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-03-15 15:19:01 +0530

    Time: 49 ms (12.73%), Space: 11.8 MB (64.66%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) 
+    {
+        if(root==nullptr)
+        {
+            return {};
+        }
+        vector<vector<int>> ans;
+        queue<Node*>q;
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int n = q.size();
+            vector<int>temp;
+            temp.clear();
+            for(int i=0;i<n;i++)
+            {
+                Node* t = q.front();
+                q.pop();
+                
+                temp.push_back(t->val);
+                
+                for(auto it:t->children)
+                {
+                    if(it!=nullptr)
+                    {
+                        q.push(it);
+                    }
+                }
+            }
+            ans.push_back(temp);
+        }
+        
+        return ans;
+        
+    }
+};
