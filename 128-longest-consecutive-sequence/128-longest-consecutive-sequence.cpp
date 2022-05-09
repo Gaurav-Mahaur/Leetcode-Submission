0a6bf56c8680a1f1100da72f114b3792f34087e7
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-05-09 20:13:58 +0530

    Time: 783 ms (14.45%), Space: 30.8 MB (67.32%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,26 +4,25 @@ public:
     {
         int n = nums.size();
         
-        unordered_set<int>st;
-        int ans=0;
+       unordered_set<int>st;
         for(int i=0;i<n;i++)
         {
             st.insert(nums[i]);
         }
-        
-       for(auto num:nums)
-       {
-           if(st.count(num-1))
-           {
-               continue;
-           }
-           int j=1;
-           while(st.count(num+j))
-           {
-               j++;
-           }
-           ans = max(ans,j);
-       }
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(st.count(nums[i]-1))
+            {
+                continue;
+            }
+            int j=1;
+            while(st.count(nums[i]+j))
+            {
+                j++;
+            }
+            ans = max(ans,j);
+        }
         return ans;
     }
 };
