a528be8a0f8b69955a7dcdde6a342b81b0fc5d9a
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-05-14 17:26:50 +0530

    Time: 7 ms (71.51%), Space: 12.8 MB (67.91%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -9,58 +9,68 @@
  */
 class Solution {
 public:
-    void making_par(TreeNode* &root, unordered_map<TreeNode*,TreeNode*>&par)
+    void makepar(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&mp)
     {
+        if(root==nullptr)
+        {
+            return;
+        }
         queue<TreeNode*>q;
         q.push(root);
         
         while(!q.empty())
         {
-            TreeNode* t = q.front();
-            q.pop();
-            
-            if(t->left)
-            {
-                q.push(t->left);
-                par[t->left]=t;
-            }
-            if(t->right)
-            {
-                q.push(t->right);
-                par[t->right]=t;
-            }
-        }
-    }
-    
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) 
-    {
-        unordered_map<TreeNode*,bool>vis;
-        unordered_map<TreeNode*,TreeNode*>par;
-        
-        making_par(root,par);
-        queue<TreeNode*>q;
-        q.push(target);
-        vis[target]=true;
-        int currlevel=0;
-        
-        while(!q.empty())
-        {
             int n = q.size();
-            if(currlevel++ == k)
-            {
-                break;
-            }
+            
             for(int i=0;i<n;i++)
             {
                 TreeNode* t = q.front();
                 q.pop();
                 
-                if(t->left && vis[t->left]!=true)
+                if(t->left)
                 {
-                   q.push(t->left);
+                    q.push(t->left);
+                    mp[t->left]=t;
+                }
+                if(t->right)
+                {
+                    q.push(t->right);
+                    mp[t->right]=t;
+                }
+            }
+        }
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) 
+    {
+        unordered_map<TreeNode*,bool>vis;
+        unordered_map<TreeNode*,TreeNode*>par;
+        
+        makepar(root,par);
+        
+        queue<TreeNode*>q;
+        q.push(target);
+        vis[target]=1;
+        int lev=0;
+        
+        while(!q.empty())
+        {
+            int n = q.size();
+            if(lev++==k)
+            {
+                break;
+            }
+            
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* t = q.front();
+                q.pop();
+                
+                if(!vis[t->left] && t->left)
+                {
+                    q.push(t->left);
                     vis[t->left]=true;
                 }
-                if(t->right && vis[t->right]!=true)
+                if(!vis[t->right] && t->right)
                 {
                     q.push(t->right);
                     vis[t->right]=true;
@@ -71,14 +81,17 @@ public:
                     vis[par[t]]=true;
                 }
             }
+            
         }
-        vector<int>ans;
+         vector<int>ans;
         while(!q.empty())
         {
             ans.push_back(q.front()->val);
             q.pop();
         }
         return ans;
+        
+        
         
     }
 };
