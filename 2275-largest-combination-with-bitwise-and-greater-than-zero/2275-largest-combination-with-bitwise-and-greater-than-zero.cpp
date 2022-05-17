fb1e7d80f46b33a49e25f0f8c37a080c150d9170
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-05-17 10:53:56 +0530

    Time: 175 ms (86.38%), Space: 57.6 MB (48.37%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) 
+    {
+        int n = candidates.size();
+        int ans=0;
+    
+        for(int i=0;i<30;i++)
+        {
+            int cnt=0;
+            for(int j=0;j<n;j++)
+            {
+                if(candidates[j]&(1<<i))
+                {
+                    cnt++;
+                }
+            }
+            ans = max(ans,cnt);
+        }
+        return ans;
+    }
+};
