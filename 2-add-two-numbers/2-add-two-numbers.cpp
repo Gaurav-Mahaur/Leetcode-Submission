351e154ce51b4719eca90fabf511b1980c1d9597
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-05-18 14:18:05 +0530

    Time: 43 ms (64.75%), Space: 71.6 MB (12.04%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -13,67 +13,74 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
     {
         ListNode* newhead = new ListNode(-1);
-        ListNode* temp = newhead;
         ListNode* t1 = l1;
         ListNode* t2 = l2;
+       // int sum=0;
         int carry=0;
+        ListNode* temp = newhead;
         while(t1 && t2)
         {
             int sum = t1->val + t2->val + carry;
+            
             if(sum>=10)
             {
-                sum  = sum%10;
-                carry = 1;
+                sum = sum%10;
+                carry =1;
             }
             else
             {
                 sum = sum%10;
-                carry = 0;
-            }
-                
-            temp->next = new ListNode(sum);
-            temp=temp->next;
-            t1 = t1->next;
-            t2=t2->next;
-        }
-        while(t1)
-        {
-            int sum  = carry+t1->val;
-            if(sum>=10)
-            {
-                sum=sum%10;
-                carry=1;
-            }
-            else
-            {
                 carry=0;
             }
             temp->next = new ListNode(sum);
             temp=temp->next;
             t1=t1->next;
+            t2=t2->next;
         }
-        while(t2)
+        while(t1)
         {
-            int sum  = carry+t2->val;
+            int sum = t1->val + carry;
+            
             if(sum>=10)
             {
-                sum=sum%10;
-                carry=1;
+                sum = sum%10;
+                carry =1;
             }
             else
             {
+                sum = sum%10;
                 carry=0;
             }
             temp->next = new ListNode(sum);
             temp=temp->next;
+            t1=t1->next;
+            //t2=t2->next;
+        }
+        while(t2)
+        {
+            int sum =  t2->val + carry;
+            
+            if(sum>=10)
+            {
+                sum = sum%10;
+                carry =1;
+            }
+            else
+            {
+                sum = sum%10;
+                carry=0;
+            }
+            temp->next = new ListNode(sum);
+            temp=temp->next;
+            //t1=t1->next;
             t2=t2->next;
         }
         if(carry==1)
         {
             temp->next = new ListNode(1);
         }
+        
         return newhead->next;
-         
         
     }
 };
