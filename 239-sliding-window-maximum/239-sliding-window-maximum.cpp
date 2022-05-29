89aee72b9e5b93b448320deb6f409f290da1c9c8
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-05-29 12:20:04 +0530

    Time: 560 ms (21.92%), Space: 134.7 MB (38.29%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -5,25 +5,32 @@ public:
         int n = nums.size();
         vector<int>ans;
         deque<int>dq;
-        for(int i=0;i<n;i++)
+        for(int i=0;i<k;i++)
         {
-            if(!dq.empty() && dq.front()==i-k)
-            {
-                dq.pop_front();
-            }
-            while(!dq.empty() && nums[i]>nums[dq.back()])
+            while(!dq.empty() && nums[dq.back()]<=nums[i])
             {
                 dq.pop_back();
             }
-            
             dq.push_back(i);
-            if(i>=k-1)
-            {
-                ans.push_back(nums[dq.front()]);
-            }
         }
-        
+        ans.push_back(nums[dq.front()]);
+        for(int i=k;i<n;i++)
+        {
+            //removal
+            while(!dq.empty() && i-dq.front()>=k)
+            {
+                dq.pop_front();
+            }
+            
+            while(!dq.empty() && nums[dq.back()]<=nums[i])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            
+            ans.push_back(nums[dq.front()]);
+        }
         return ans;
-    
+        
     }
 };
