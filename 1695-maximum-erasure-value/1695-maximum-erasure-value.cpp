d0fd9e275128cc7412e1d049de869e4f4d8bfde9
Gaurav Mahaur <93429968+Gaurav-Mahaur@users.noreply.github.com>
2022-06-12 13:03:29 +0530

    Time: 419 ms (67.32%), Space: 103.5 MB (61.73%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -4,26 +4,47 @@ public:
     {
         int n = nums.size();
         
-        set<int>st;
-        int j=0;
-        int i=0;
-        int currsum=0;
-        int ans = INT_MIN;
-        while(i<n)
+        unordered_map<int,int>mp;
+        vector<int>pref(n,0);;
+        for(int i=0;i<n;i++)
         {
-            while(st.find(nums[i])!=st.end() && j<i)
+            if(i==0)
             {
-                currsum -= nums[j];
-                st.erase(nums[j]);
-                j++;
+                pref[i]=nums[i];
+            }
+            else
+            {
+                pref[i] = pref[i-1]+nums[i];
+            }
+        }
+        int j=0;
+        int currsum=0;
+        int ans =0;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(nums[i])!=mp.end())
+            {
+                if(mp[nums[i]]>=j)
+                {
+                    
+                    if(j==0)
+                    {
+                        currsum -= pref[mp[nums[i]]];
+                    }
+                    else
+                    {
+                        currsum -= pref[mp[nums[i]]]-pref[j-1];
+                    }
+                    j = mp[nums[i]]+1;
+                    
+                }
+                
+                
             }
             currsum += nums[i];
+            mp[nums[i]]=i;
             ans = max(ans,currsum);
-            
-            st.insert(nums[i]);
-            i++;
         }
         return ans;
-        
     }
 };
